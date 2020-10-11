a98515b2e167332f162bde18afaaa931acb87bf9
jonasdevzero <jonasdevzero@gmail.com>
2020-10-10 22:43:05 -0300

    building compound components of  header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+    Container,
+    Logo,
+    Search,
+    SearchInput,
+    Navegation,
+    Option,
+    OptionLineOne,
+    OptionLineTwo
+} from './styles';
 
-function Header() {
-    return (
-        <Container>
+function Header({ children, ...props }) {
+    return <Container {...props}>{children}</Container>
+};
 
-        </Container>
-    );
+Header.Logo = function HeaderLogo() {
+    return <Logo src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />;
+};
+
+Header.Search = function HeaderSearch({ children, ...props }) {
+    return <Search {...props}>{children}</Search>;
+};
+
+Header.SearchInput = function HeaderSearchInput({ children, ...props }) {
+    return <SearchInput {...props}>{children}</SearchInput>;
+};
+
+Header.Navegation = function HeaderNavegation({ children, ...props }) {
+    return <Navegation {...props}>{children}</Navegation>;
+};
+
+Header.Option = function HeaderOption({ cjildren, ...props }) {
+    return <Option {...props}>{children}</Option>;
+};
+
+Header.OptionLineOne = function HeaderOptionLineOne({ children, ...props }) {
+    return <OptionLineOne {...props}>{children}</OptionLineOne>;
+};
+
+Header.OptionLineTwo = function HeaderOptionLineTwo({ children, ...props }) {
+    return <OptionLineTwo {...props}>{children}</OptionLineTwo>;
 };
 
 export default Header;
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,33 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const Container = styled.div`
+
+`;
+
+export const Logo = styled.img`
+
+`;
+
+export const Search = styled.div`
+
+`;
+
+export const SearchInput = styled.input`
+
+`;
+
+export const Navegation = styled.div`
+
+`;
+
+export const Option = styled.div`
+
+`;
+
+export const OptionLineOne = styled.span`
+
+`;
+
+export const OptionLineTwo = styled.span`
 
 `
