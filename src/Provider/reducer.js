859d2b3f1f674491021f0a8b84b75256dfc1d493
jonasdevzero <jonasdevzero@gmail.com>
2020-10-31 17:05:32 -0300

    feat: Adding state provider

---
+++ src/Provider/reducer.js
@@ -0,0 +1,17 @@
+export const initialState = {
+    basket: [],
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'ADD_TO_BASKET': 
+            return { 
+                ...state, 
+                basket: [...state.basket, action.item] 
+            }
+        default: 
+            return;
+    };
+};
+
+export default reducer;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import { GlobalStyles } from './GlobalStyles';
+import { StateProvider } from './Provider/StateProvider';
+import reducer, { initialState } from './Provider/reducer';
 
 ReactDOM.render(
-  <>
+  <StateProvider initialState={initialState} reducer={reducer}>
     <GlobalStyles />
     <App />
-  </>
+  </StateProvider>
   ,document.getElementById('root')
 );
