4f3d3c666e162b6a84e03c7c958ec69fa044eecd
jonasdevzero <jonasdevzero@gmail.com>
2020-10-30 23:12:39 -0300

    feat: Building Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { 
+    HeaderContainer,
+    HomeContainer  
+} from '../containers'
+
+function Home() {
+    return (
+        <div>
+            <HeaderContainer />
+            <HomeContainer />
+        </div>
+    );
+};
+
+export default Home;
