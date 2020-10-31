4bb624fbe21eb68e0b6d5af7bad038048e3402b8
jonasdevzero <jonasdevzero@gmail.com>
2020-10-31 17:06:50 -0300

    refact

--- src/containers/HomeContainer.jsx
+++ src/containers/HomeContainer.jsx
@@ -12,73 +12,49 @@ function HomeContainer() {
                 />
 
                 <Home.Row>
-                    <Product>
-                        <Product.Info>
-                            <Product.Title>Play Station 5</Product.Title>
-                            <Product.Price>500.0</Product.Price>
-                        </Product.Info>
-                        <Product.Rating rating={5} />
-                        <Product.Image 
-                            src="https://s2.glbimg.com/I2_W4j1HEKEtrCj-bgTPVMzN5S0=/0x236:3600x2211/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/L/W/GO8WsPQ6C090NUUUD2GA/ps5.jpg" 
-                            alt="play station 5" 
-                        />
-                        <Product.Add />
-                    </Product>
-                    <Product>
-                        <Product.Info>
-                            <Product.Title>Xbox series X</Product.Title>
-                            <Product.Price>450.0</Product.Price>
-                        </Product.Info>
-                        <Product.Rating rating={5} />
-                        <Product.Image src="https://imagens.canaltech.com.br/produto/1584982530-7573-principal-m.png" 
-                            alt="xbox series x" 
-                        />
-                        <Product.Add />
-                    </Product>
+                    <Product
+                        title='Play Station 5'
+                        price={500.0}
+                        rating={5}
+                        image="https://s2.glbimg.com/I2_W4j1HEKEtrCj-bgTPVMzN5S0=/0x236:3600x2211/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/L/W/GO8WsPQ6C090NUUUD2GA/ps5.jpg"
+                    />
+                    <Product
+                        title='Xbox series X'
+                        price={450.0}
+                        rating={5}
+                        image='https://imagens.canaltech.com.br/produto/1584982530-7573-principal-m.png'
+                    />
                 </Home.Row>
 
                 <Home.Row>
-                    <Product>
-                        <Product.Info>
-                            <Product.Title>Xiaomi MI 9T</Product.Title>
-                            <Product.Price>870.5</Product.Price>
-                        </Product.Info>
-                        <Product.Rating rating={4} />
-                        <Product.Image src="https://imagens.canaltech.com.br/produto/1562787521-6925-principal-m.png" alt="xiaomi mi 9T" />
-                        <Product.Add />
-                    </Product>
-                    <Product>
-                        <Product.Info>
-                            <Product.Title>Iphone X</Product.Title>
-                            <Product.Price>1000.0</Product.Price>
-                        </Product.Info>
-                        <Product.Rating rating={4} />
-                        <Product.Image src="https://www.freeiconspng.com/thumbs/iphone-x-pictures/apple-iphone-x-pictures-5.png" alt="Iphone X" />
-                        <Product.Add />
-                    </Product>
-                    <Product>
-                        <Product.Info>
-                            <Product.Title>Sansung S20</Product.Title>
-                            <Product.Price>1.339.0</Product.Price>
-                        </Product.Info>
-                        <Product.Rating rating={5} />
-                        <Product.Image src="https://static.carrefour.com.br/medias/sys_master/images/images/h46/hb9/h00/h00/26477859438622.jpg" alt="Sansung S20" />
-                        <Product.Add />
-                    </Product>
+                    <Product
+                        title='Xiaomi MI 9T'
+                        price={870.5}
+                        rating={4}
+                        image='https://imagens.canaltech.com.br/produto/1562787521-6925-principal-m.png'
+                    />
+                    <Product
+                        title='Iphone X'
+                        price={1000.0}
+                        rating={4}
+                        image='https://www.freeiconspng.com/thumbs/iphone-x-pictures/apple-iphone-x-pictures-5.png'
+                    />
+                    <Product
+                        title='Sansung S20'
+                        price={1339.0}
+                        rating={5}
+                        image='https://static.carrefour.com.br/medias/sys_master/images/images/h46/hb9/h00/h00/26477859438622.jpg'
+                    />
                 </Home.Row>
 
                 <Home.Row>
-                <Product>
-                        <Product.Info>
-                            <Product.Title>God of War</Product.Title>
-                            <Product.Price>20.0</Product.Price>
-                        </Product.Info>
-                        <Product.Rating rating={5} />
-                        <Product.Image src="https://cdn.awsli.com.br/600x450/138/138431/produto/28660383/cf49edd8c9.jpg" alt="God of War" />
-                        <Product.Add />
-                    </Product>
+                    <Product
+                        title='God of War'
+                        price={20.0}
+                        rating={5}
+                        image='https://cdn.awsli.com.br/600x450/138/138431/produto/28660383/cf49edd8c9.jpg'
+                    />
                 </Home.Row>
-
             </Home.Inner>
         </Home>
     );
