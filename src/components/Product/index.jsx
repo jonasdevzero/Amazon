33bed285b44acece8bf0408c362a3a8c8fa048e3
jonasdevzero <jonasdevzero@gmail.com>
2020-10-31 16:17:29 -0300

    refactor

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -11,40 +11,24 @@ import {
 } from './styles';
 import StarRateIcon from '@material-ui/icons/StarRate';
 
-function Product({ children, ...props }) {
-    return <Container {...props}>{children}</Container>;
-};
-
-Product.Info = function ProductInfo({ children, ...props }) {
-    return <Info {...props}>{children}</Info>;
-};
-
-Product.Title = function ProductTitle({ children, ...props }) {
-    return <Title {...props}>{children}</Title>;
-};
-
-Product.Price = function ProductPrice({ children, ...props }) {
+function Product({ title, price, rating, image, onClick }) {
     return (
-        <Price {...props}>
-            <small>$</small>
-            <strong>{children}</strong>
-        </Price>);
-};
+        <Container>
+            <Info>
+                <Title>{title}</Title>
+                <Price>
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </Price>
+            </Info>
+            <Rating>
+                {Array(rating).fill().map(_ => <StarRateIcon fontSize="large" />)}
+            </Rating>
+            <Image src={image} alt='product' />
 
-Product.Rating = function ProductRating({ rating }) {
-    return (
-        <Rating>
-            {Array(rating).fill().map(_ => <StarRateIcon fontSize="large" />)}
-        </Rating>
+            <Add onClick={onClick}>Add to basket</Add>
+        </Container>
     );
 };
-
-Product.Image = function ProductImage({ src, alt }) {
-    return <Image src={src} alt={alt} />
-}
-
-Product.Add = function ProductAdd({ ...props }) {
-    return <Add {...props}>Add to basket</Add>
-}
 
 export default Product;
