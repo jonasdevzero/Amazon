bc2cd99eaa68ba11d11292f521fd8244441a0a05
jonasdevzero <jonasdevzero@gmail.com>
2020-11-06 23:39:02 -0300

    feat: Adding user listener and set user method

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { auth } from './firebase';
+import { useStateValue } from './Provider/StateProvider';
 
 import { Home, Checkout, Login } from './pages';
 
 function Routes() {
+    const [{}, dispatch] = useStateValue();
+
+    useEffect(() => {
+        auth.onAuthStateChanged(authUser => {
+            if (authUser) {
+                dispatch({
+                    type: 'SET_USER',
+                    user: authUser
+                })
+            } else {
+                dispatch({
+                    type: 'SET_USER',
+                    user: null
+                });
+            };
+        });
+    }, []);
+
     return (
         <Router>
             <Switch>
