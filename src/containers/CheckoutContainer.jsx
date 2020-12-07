e9a8333e55d4569d4dd92e2961d41a1fa5703cfd
jonasdevzero <jonasdevzero@gmail.com>
2020-12-07 19:29:23 -0300

    feat

--- src/containers/CheckoutContainer.jsx
+++ src/containers/CheckoutContainer.jsx
@@ -5,16 +5,17 @@ import { useStateValue } from '../Provider/StateProvider';
 import { Checkout, Subtotal } from '../components';
 
 function CheckoutContainer() {
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }] = useStateValue();
 
     return (
         <Checkout>
             <Checkout.Left>
-                <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="ad"/>
+                <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="ad" />
 
                 <div>
+                    <h2>Hello, {user?.email}</h2>
                     <Checkout.Title>Your shopping Basket</Checkout.Title>
-                    {basket.map((item, i) => <Checkout.Product key={`${item.title}-${i}` } item={item} />)}
+                    {basket.map((item, i) => <Checkout.Product key={`${item.title}-${i}`} item={item} />)}
                 </div>
             </Checkout.Left>
 
