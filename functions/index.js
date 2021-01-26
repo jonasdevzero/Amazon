2df31b80cff89a79562319704e8d233118ed11ce
jonasdevzero <jonasdevzero@gmail.com>
2021-01-25 21:45:21 -0300

    initializing the cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')('sk_test_51I98HZC464Xu6X2cwxFgCw8BXeRpt7VWfeHNbfyln1w0E7oCIBYAQ3t7HgnOOVdWTnz8PmzZbp0MDOYhTk9zRugH00rZ8CIrfc');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get('/', (request, response) => response.status(200).json('hello world'));
+
+exports.api = functions.https.onRequest(app);
+
+// http://localhost:5001/devzero-831f4/us-central1/api
